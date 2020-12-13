dbfe7f73a0317e84859465948dff94b3cb52e3a4
Arturo <arturo@inspiron.domain.name>
2020-12-12 23:37:47 -0600

    Añadir templates básicos de CEDIS y TIIE, views y url's incluidas.

--- consulta_cedis/urls.py
+++ consulta_cedis/urls.py
@@ -3,4 +3,7 @@ from django.urls import include, re_path
 
 urlpatterns = [
     re_path(r'^$', views.index, name='index'),
+    re_path(r'^consulta_cedis/$', views.consulta_cedis, name='consulta_cedis'),
+    re_path(r'^consulta_tiie/$', views.consulta_tiie, name='consulta_tiie'),
+    
 ]  
--- consulta_cedis/views.py
+++ consulta_cedis/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'home/home.html')
+
+def consulta_cedis(request):
+    return render(request,'cedis/home.html')
+
+def consulta_tiie(request):
+    return render(request,'tiie/home.html')

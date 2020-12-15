ee9749a1c4ed7afcb894f465340587e2a99a89ca
Arturo <arturo@inspiron.domain.name>
2020-12-14 18:49:27 -0600

    Página de consulta de cedis y dólar.

--- consulta_cedis/views.py
+++ consulta_cedis/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
 def index(request):
     return render(request,'home/home.html')
 
-def consulta_cedis(request):
-    return render(request,'cedis/home.html')
+def consulta_udis(request):
+    hoy = datetime.today().strftime("%d-%m-%Y")
+    return render(request,'udis/home.html',{'hoy':hoy})
 
 def consulta_tiie(request):
-    return render(request,'tiie/home.html')
+    return render(request,'tiie/home.html',{'hoy':hoy})
